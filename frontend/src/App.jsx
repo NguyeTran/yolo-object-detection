32940162700fddb1b09c88adf6d3ff1f598dc4e4
Thanh-v <thanhvinh011206@gmail.com>
2026-08-10 17:08:14 +0700

    Change the frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,81 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+import { useState, useRef } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // 1. State to save data
+
+  //Save picture user uploaded
+  const [selectedFile, setSelectedFile] = useState(null);
+
+  //Save the temporary URL of the uploaded picture
+  const [previewUrl, setPreviewUrl] = useState(null);
+
+  //Notice: AI is thinking?
+  const [isLoading, setIsLoading] = useState(false);
+
+  //Result from FastAPI
+  const [result, setResult] = useState(null);
+
+  //Display the picture to drawing the bounding box
+  const imageRef = useRef(null);
+
+  //2. Function to handle file selection
+
+  const handleFileChange = (event) => {
+    // Get the selected file from the input
+    const file = event.target.files[0];
+    if (file) {
+      // Update the selected file state
+      setSelectedFile(file);
+
+      // Create a temporary URL for the selected file to display it
+      setPreviewUrl(URL.createObjectURL(file));
+
+      // Reset the result state when a new file is selected
+      setResult(null);
+    }
+  };
+
+  //3. Function to handle the upload and send the file to FastAPI
+
+  const handleUpload = async () => {
+    // Check if a file is selected
+    if (!selectedFile) {
+      alert('Please select a file first.');
+      return;
+    }
+
+    // Set loading state to true
+    setIsLoading(true);
+
+    // Create a FormData object to send the file
+    const formData = new FormData();
+    formData.append('file', selectedFile);
+    
+    try {
+      // Send the file to FastAPI using fetch
+      const response = await fetch('http://127.0.0.1:8000/api/detect/image', {
+        method: 'POST',
+        body: formData,
+      });
+    
+      // Check if the response is OK
+      if (!response.ok) {
+        throw new Error('Failed to upload the file.');
+      }
+      
+      //Parse the JSON response from FastAPI
+      const data = await response.json();
+      setResult(data);
+    }
+    catch (error) {
+      console.error('Error uploading file:', error);
+      alert('Error uploading file. Please try again.');
+    } finally {
+      // Set loading state to false
+      setIsLoading(false);
+    }
+  };
 
   return (
     <>
